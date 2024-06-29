3c035b487467d3ab20cd8c730c7174b89d822cd5
xwaldo <livexwaldo@gmail.com>
2024-06-29 03:32:29 -0600

    A

--- src/Plugin/PluginApp.lua
+++ src/Plugin/PluginApp.lua
@@ -1,84 +1,64 @@
+local CoasterCreator = require(script.Parent.Parent.Coaster.CoasterCreator)
 local CoasterHeader = require(script.Parent.Parent.Coaster.CoasterHeader)
 local Fusion = require(script.Parent.Parent.Packages.Fusion)
 local Styles = require(script.Parent.Parent.Styles)
 local PluginHeader = require(script.Parent.PluginHeader)
 
-return function(widget)
-    local CurrentCoaster = Fusion.Value(true)
-
-    local element = Fusion.New("Frame"){
-        Parent = widget,
-        BackgroundTransparency = 1,
-        Size = UDim2.fromScale(1,1),
-        [Fusion.Children] = {
-            Fusion.New("UIListLayout"){
-                
-            },
-            PluginHeader{
-                CurrentCoaster = CurrentCoaster
-            },
-            Fusion.New("Frame"){
-                Visible = Fusion.Computed(function()  
-                    return if CurrentCoaster:get() then false else true
-                end),
-                BackgroundTransparency = 1,
-                Size = UDim2.new(1,0,0,0),
-                [Fusion.Children] = {
-                    Fusion.New("UIListLayout"){
-                        VerticalAlignment = Enum.VerticalAlignment.Center
-                    },
-                    Fusion.New("Frame"){
-                        BackgroundTransparency = 1,
-                        Size = UDim2.new(1,0,0.5,0),
-                        [Fusion.Children] = {
-                            Fusion.New("UIListLayout"){},
-                            Fusion.New("TextLabel"){
-                                LayoutOrder = 0,
-                                Text = "Create new coaster",
-                                TextColor3 = Styles.BasicText.TextColor3,
-                                Font = Styles.BasicText.Font,
-                                BackgroundTransparency = 1,
-                                Size = UDim2.new(1,0,0,20),
-                            },
-                            Fusion.New("TextLabel"){
-                                LayoutOrder = 0,
-                                Text = "Name:",
-                                TextColor3 = Styles.BasicText.TextColor3,
-                                Font = Enum.Font.SourceSans,
-                                BackgroundTransparency = 0,
-                                Position = UDim2.new(),
-                                Size = UDim2.new(1,-4,0,20),
-                            }
-                        },
-                    },
-                    Fusion.New "UIFlexItem" {
-                        FlexMode = Enum.UIFlexMode.Fill
-                    },
-                }
-
-            },
-            Fusion.New("Frame"){
-                Visible = Fusion.Computed(function()  
-                    return if CurrentCoaster:get() then true else false
-                end),
-                BackgroundTransparency = 1,
-                Size = UDim2.new(1,0,0,0),
-                [Fusion.Children] = {
-                    Fusion.New("UIListLayout"){},
-                    Fusion.New "UIFlexItem" {
-                        FlexMode = Enum.UIFlexMode.Fill
-                    },
-                    CoasterHeader{
-                        CurrentCoaster = CurrentCoaster
-                    }
-                }
-
-            }
-        }
+local function LoadCoaster(Folder)
+    local Name = Folder.Name
+    local CreatedTime = Folder.CreatedTime.Value
+    local Points = Fusion.ForValues(Folder.Points:GetChildren(), function(Points)  
+        return {}--LoadPoints(Points)
+    end)
+    return {
+        Name = Name,
+        CreatedTime = CreatedTime,
+        Points = Points
     }
-
-    return function()
-        element:Destroy()
-    end
 end
 
+return function(widget)
+	local CurrentCoaster = Fusion.Value(nil)
+    local Coasters = Fusion.Value(Fusion.ForPairs(GetCoasterFolders(), function(_,folder)  
+		return LoadCoaster(folder)
+	end))
+	local CoastersToSelect = Fusion.ForPairs(Coasters, function(_, coaster)  
+		return coaster.TimeCreated, coaster.name
+	
+	end)
+	local element = Fusion.New("Frame")({
+		Parent = widget,
+		BackgroundTransparency = 1,
+		Size = UDim2.fromScale(1, 1),
+		[Fusion.Children] = {
+			Fusion.New("UIListLayout")({}),
+			PluginHeader({
+				Coasters = CoastersToSelect,
+				CurrentCoaster = CurrentCoaster,
+			}),
+            CoasterCreator({
+                CurrentCoaster = CurrentCoaster,
+            }),
+			Fusion.New("Frame")({
+				Visible = Fusion.Computed(function()
+					return CurrentCoaster:get() ~= nil
+				end),
+				BackgroundTransparency = 1,
+				Size = UDim2.new(1, 0, 0, 0),
+				[Fusion.Children] = {
+					Fusion.New("UIListLayout")({}),
+					Fusion.New("UIFlexItem")({
+						FlexMode = Enum.UIFlexMode.Fill,
+					}),
+					CoasterHeader({
+						CurrentCoaster = CurrentCoaster,
+					}),
+				},
+			}),
+		},
+	})
+    
+	return function()
+		element:Destroy()
+	end
+end
